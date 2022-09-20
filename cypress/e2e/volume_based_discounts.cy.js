eaad9887622a577cd2d88fdcfbaf78f53d15bc82
Spencer Horn <spencer.horn@davinci-ed.com>
2022-09-20 14:11:52 -0400

    Completed test for 5 items volume discount

--- cypress/e2e/volume_based_discounts.cy.js
+++ cypress/e2e/volume_based_discounts.cy.js
@@ -1,9 +1,22 @@
-describe('As a buyer, I want to get a discount when I purchase five items of the same product so that Im incentivized to buy more.', () => {
-  it('navigates to the everly homepage', () => {
+describe('Discounts', () => {
+  beforeEach(() => {
     cy.visit('https://www.everlywell.com')
-    cy.title().should('eq', 'Everlywell - Innovative at-home Health Testing')
   })
-  it('clicks on Food Sensitivity Comprehensive Test', () => {
-    cy.get('.a').contains('Shop our products').click
+
+  context("Volume based discounts", () => {
+
+    it('As a buyer, I want to get a discount when I purchase five items of the same product so that I am incentivized to buy more.', () => {
+      // Launch and confirm on landing page
+      cy.title().should('eq', 'Everlywell - Innovative at-home Health Testing')
+      // Click on Food Sensitivity Comprehensive Test
+      cy.getByDataId("productCard").eq(0).click()
+      // Select and confirm quantitiy of 5 from quantity drop down
+      cy.getByDataId("productQuantity").eq(0).select(4).should('have.value', 5)
+      // Click add to cart
+      cy.get('[data-testid="addToCartButton"]').click()
+      // Confirm cart Subtotal equals that of one test
+      cy.get('[data-testid="cart-subtotal"]').contains('$299.00')
+    })
+
   })
 })
